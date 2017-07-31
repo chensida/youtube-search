1ad142c0b24550773ac2a9d2a24086b1bb3fae88
sidac <sida.chen@veeva.com>
2017-07-31 14:11:50 -0400

    fixed typo

--- php/display.php
+++ php/display.php
@@ -17,7 +17,7 @@ echo "<table>";
 echo "<tr><td>title</td><td>date</td><td>description</td><td>channel</td><td>tags</td><td>duration</td><td>topicDetails</td></tr>"; 
 
 foreach ($all as $data) {
-	echo "<tr><td>" . $row['title'] . "</td><td>" . $row['date'] . "</td><td>" . $row['description'] . "</td><td>" . $row['channel'] . "</td><td>" . $row['tags'] . "</td><td>" . $row['duration'] . "</td><td>" . $row['topicDetails'] . "</td></tr>"; 
+	echo "<tr><td>" . $data['title'] . "</td><td>" . $data['date'] . "</td><td>" . $data['description'] . "</td><td>" . $data['channel'] . "</td><td>" . $data['tags'] . "</td><td>" . $data['duration'] . "</td><td>" . $data['topicDetails'] . "</td></tr>"; 
 }
 
 echo "</table>"; 
